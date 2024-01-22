11a639d52794f4076c0629bd7a297696898e08cd
Enyata <adeyoolaiyanuoluwa@gmail.com>
2024-01-22 11:08:00 +0100

    worked on side bar component

---
+++ src/component/SideBar/navLinks.ts
@@ -0,0 +1,21 @@
+import DashboardIcon from "../../assets/dashboard.svg";
+import HistoryIcon from '../../assets/history.svg'
+import UserIcon from '../../assets/user.svg'
+
+export const navLinks = [
+  {
+    title: "Dashboard",
+    icon: DashboardIcon,
+    url: '/dashboard'
+  },
+  {
+    title: "Clock-in History",
+    icon: HistoryIcon,
+    url: '/history'
+  },
+  {
+    title: "User Management",
+    icon: UserIcon,
+    url: '/user-management'
+  },
+];
