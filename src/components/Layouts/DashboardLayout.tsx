e2f08057ccfabcf2e8e79e158a72dcd702094bb5
Enyata <adeyoolaiyanuoluwa@gmail.com>
2024-01-31 15:20:02 +0100

    dashboard screen conversion

--- src/components/Layouts/DashboardLayout.tsx
+++ src/components/Layouts/DashboardLayout.tsx
@@ -4,17 +4,19 @@ import SideBar from "../SideBar";
 import classNames from "classnames";
 import Header from "../Header";
 import { ChildrenProps } from "../../types";
+import SmallScreenHeader from "../Header/SmallScreenHeader";
 
 const DashboardLayout = ({ children }: ChildrenProps) => {
   return (
     <main className={classNames(styles.main__layout)}>
       <SideBar />
       <section className={styles.main__page}>
-        <Header />
+       <div className={styles.main__mainscreen}> <Header /></div>
+        <div className={styles.main__smallscreen}>
+          <SmallScreenHeader />
+        </div>
         <>
-          <div className={styles.main__content}>
-            {children}
-            </div>
+          <div className={styles.main__content}>{children}</div>
         </>
       </section>
     </main>
--- src/container/Dashboard/Dashboard.tsx
+++ src/container/Dashboard/Dashboard.tsx
@@ -1,16 +1,95 @@
-import React from 'react'
-import styles from './dashboard.module.scss'
-import DashboardLayout from '@/components/Layouts/DashboardLayout'
-import Card from '@/components/Card'
+import React from "react";
+import styles from "./dashboard.module.scss";
+import DashboardLayout from "@/components/Layouts/DashboardLayout";
+import Card from "@/components/Card";
+import Avatar from "@/components/Avatar";
+import { history, membersData } from "@/mocks";
+import PlaceholderIcon from "../../assets/placeholder.svg";
+import { useNavigate } from "react-router";
 
 const Dashboard = () => {
+  const navigate = useNavigate()
   return (
-    <div>
-        <DashboardLayout>
-            <Card/>
-        </DashboardLayout>
-    </div>
-  )
-}
+    <DashboardLayout>
+      <Card />
+      <div className={styles.dashboard}>
+        <div className={styles.dashboard__members}>
+          <div className={styles.dashboard__members__header}>
+            <text>Recently added members</text>
+            <p onClick={()=>navigate('/user-management')}>See more</p>
+          </div>
+          {membersData.slice(0, 5).map((i) => (
+            <div className={styles.dashboard__members__data}>
+              <div className={styles.dashboard__members__data__avatar}>
+                <Avatar name={i.last_name} />
+                <div>
+                  <text>
+                    {i.first_name} {i.last_name}
+                  </text>
+                  <p>{i.address}</p>
+                </div>
+              </div>
+              <div>
+                <p>Date added:</p>
+                <p style={{ color: "#1F2937" }}>{i.date}</p>
+              </div>
+            </div>
+          ))}
+          {!membersData?.length && (
+            <>
+              <div className={styles.dashboard__members__empty}>
+                <div>
+                  {<img src={PlaceholderIcon} alt="" />}{" "}
+                  <p>You have no recent activity.</p>
+                  <p>Go to “User management” to add members</p>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
 
-export default Dashboard
+        <div className={styles.dashboard__members}>
+          <div className={styles.dashboard__members__header}>
+            <text>Recent clock-ins</text>
+            <p onClick={()=>navigate('/history')}>See more</p>
+          </div>
+
+          {history.slice(0, 5).map((i) => (
+            <div className={styles.dashboard__members__data}>
+              <div className={styles.dashboard__members__data__avatar}>
+                <Avatar name={i.full_name} />
+                <div>
+                  <text>{i.full_name}</text>
+                  <p>{i.address}</p>
+                </div>
+              </div>
+              <div>
+                <p>Clock-in:</p>
+                <p style={{ color: "#1F2937" }}>
+                  {i.date}{" "}
+                  <span className={styles.dashboard__divider}>
+                    {i.clock_in}
+                  </span>
+                </p>
+              </div>
+            </div>
+          ))}
+
+          {!history?.length && (
+            <>
+              <div className={styles.dashboard__members__empty}>
+                <div>
+                  {<img src={PlaceholderIcon} alt="" />}{" "}
+                  <p>You have no recent activity.</p>
+                  <p>Go to “User management” to add members</p>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default Dashboard;
