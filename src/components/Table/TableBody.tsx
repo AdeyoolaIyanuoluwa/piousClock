b98de73a9d3c4837e8149f25f37a201b2de3b6bb
Enyata <adeyoolaiyanuoluwa@gmail.com>
2024-01-31 15:22:49 +0100

    worked on table component

--- src/components/Table/TableBody.tsx
+++ src/components/Table/TableBody.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import styles from './table.module.scss';
 import { TableProps } from '@/types';
 
-const TableBody = ({ tableData, content }: TableProps) => (
+const TableBody = ({ tableData, content }: TableProps) => {
+  return(
   <tbody className="table__body">
-  
-    {tableData?.map((row, index) => (
-      <tr className={styles.table__row} key={index}>
+    
+    {tableData?.map((row, index) => {
+      return (
+        <tr className={styles.table__row} key={index}>
         <>{content(row)}</>
       </tr>
-    ))}
+      )
+    })}
   </tbody>
-);
+)};
 
 
 export default TableBody;
